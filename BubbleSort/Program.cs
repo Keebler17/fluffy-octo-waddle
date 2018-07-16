b7377ddc9bb8eda16625d889d72f1fe9e13e7903
Keebler17 <borkerdoggo493@gmail.com>
2018-07-15 20:09:49 -0400

    I think this will add the files? I sorta broke it with the .gitignore thing.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort {
+	class Program {
+		static void Main(string[] args) {
+			int[] unsorted = { 51, 22, 99, 83, 42, 4, 7, 22, 46, 83, 22, 79, 36, 38, 91, 128 };
+			int[] sorted = new int[unsorted.Length];
+
+			Console.WriteLine("Unsorted list: " + unsorted);
+
+
+			Console.Read();
+		}
+	}
+}
